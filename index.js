d44ac4e7b98121d43b9b5a20bdd3e7d4aa583288
Dane <Dseatondesign@gmail.com>
2022-07-30 15:32:31 +1000

    completed assignemtn

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-document.addEventListener("DOMContentloaded", (e) => {
-  // invoking a function here will make sure it happens on page load
-  // your code here
+document.addEventListener('DOMContentloaded', (e) => {
 });
+
+function displayMovie(movie) {
+    document.getElementById("title").innerHTML = movies[movie].title;
+    document.getElementById("director").innerHTML = movies[movie].director;
+    document.getElementById("genre").innerHTML = movies[movie].genre;
+    document.getElementById("filmRating").innerHTML = movies[movie].filmRating;
+    document.getElementById("cast").innerHTML = movies[movie].cast.map(item => `${item.actor}-${item.role}`);
+    document.getElementById("poster").src = movies[movie].poster;
+}
+displayMovie("Titanic")
+document.getElementById('Titanic')
+.addEventListener('click', () => displayMovie ("Titanic"));
+document.getElementById('Terminator 2')
+.addEventListener('click', () => displayMovie ("Terminator 2"));
